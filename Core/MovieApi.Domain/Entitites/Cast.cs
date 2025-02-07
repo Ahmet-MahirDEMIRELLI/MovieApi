4261d858f7655ba74b70cc8c8f3d1066624b779f
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-07 12:53:24 +0300

    Lesson 5 completed

---
+++ Core/MovieApi.Domain/Entitites/Cast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entitites
+{
+    public class Cast
+    {
+        public int CastId { get; set; }
+        public required string Title { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string ImageUrl { get; set; }
+        public string? Overivew { get; set; }
+        public string? Biography { get; set; }
+    }
+}
