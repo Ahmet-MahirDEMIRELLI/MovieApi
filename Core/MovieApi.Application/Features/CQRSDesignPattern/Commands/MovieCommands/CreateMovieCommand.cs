973376339d7affc9494920337386978934591c48
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-11 21:50:11 +0300

    Lesson 8 completed

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/MovieCommands/CreateMovieCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands
+{
+    public class CreateMovieCommand
+    {
+        public required string MovieTitle { get; set; }
+        public required string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public required string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public required string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/CreateMovieCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.CategoryCommands;
+using MovieApi.Persistence.Context;
+using MovieApi.Domain.Entitites;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class CreateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public CreateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handle(CreateMovieCommand command)
+        {
+            _context.Movies.Add(new Movie 
+            {
+                CoverImageUrl = command.CoverImageUrl,
+                CreatedYear = command.CreatedYear,
+                Description = command.Description,
+                Duration = command.Duration,
+                Rating = command.Rating,
+                ReleaseDate = command.ReleaseDate,
+                Status = command.Status,
+                MovieTitle = command.MovieTitle
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
