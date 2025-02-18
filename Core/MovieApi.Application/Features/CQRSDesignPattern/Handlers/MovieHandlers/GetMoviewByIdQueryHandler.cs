3c45a1231b186f2941f402850bcb705c6e3c9baf
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-18 22:53:18 +0300

    Lesson 10 completed

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMoviewByIdQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Queries.CategoryQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.CategoryResults;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMoviewByIdQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMoviewByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async Task<GetMovieByIdQueryResult> Handle(GetMovieByIdQuery query)
+        {
+            var value = await _context.Movies.FindAsync(query.MovieId);
+            return new GetMovieByIdQueryResult
+            {
+                CoverImageUrl = value.CoverImageUrl,
+                CreatedYear = value.CreatedYear,
+                Description = value.Description,
+                Duration = value.Duration,
+                Rating = value.Rating,
+                ReleaseDate = value.ReleaseDate,
+                Status = value.Status,
+                MovieTitle = value.MovieTitle,
+            };
+        }
+    }
+}
